ac24db317d4703c12761fd244b8bf1626f50f5fa
Hugo Armando Tellez Velasco <195217387+htellezv@users.noreply.github.com>
2026-07-13 19:12:11 -0500

    Create public library API

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { MotionEngine } from './core/MotionEngine';
+export type { MotionFrame } from './core/MotionEngine';
+export type { MotionInput } from './core/MotionInput';
+export { MotionProvider, useMotionEngine, useMotionFrame } from './providers';
+export { InteractiveVideo } from './components/InteractiveVideo';
